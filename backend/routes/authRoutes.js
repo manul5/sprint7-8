1e3cde9850a5644dd1b36be9d0897cf71d897127
412152_giorda_brunella <412152@tecnicatura.frc.utn.edu.ar>
2025-11-10 10:06:13 -0300

    corrección back

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,8 @@
-import express from "express";
-import { registerUser } from "../controllers/authController.js";
-
+const express = require("express");
+const { registerUser } = require("../controllers/authController");
 const router = express.Router();
 
 // Endpoint: POST /api/auth/register
 router.post("/register", registerUser);
 
-export default router;
+module.exports = router;
